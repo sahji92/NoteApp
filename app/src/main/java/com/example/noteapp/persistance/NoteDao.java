5f0452bd075489dfc16fd94997b5f002ae56ed70
dipak <sahdeepak330@gmail.com>
2020-12-12 16:40:21 +0530

    creating NoteDatabase class and NoteDAO interface

---
+++ app/src/main/java/com/example/noteapp/persistance/NoteDao.java
@@ -0,0 +1,26 @@
+package com.example.noteapp.persistance;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.noteapp.model.Note;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+    @Insert
+    void insert(Note note);
+    @Update
+    void update(Note note);
+    @Delete
+    void delete(Note note);
+    @Query("DELETE FROM note_table")
+    void deleteAllNotes();
+    @Query("select * from note_table order by priority desc")
+    LiveData<List<Note>> getAllNotes();
+}
