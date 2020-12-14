7169048bd24ecdc6fae0b96d6d985d18d07f1ed7
dipak <sahdeepak330@gmail.com>
2020-12-14 16:24:01 +0530

    creating repository class,asynctasks,and Roomcallback

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.noteapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView webView=findViewById(R.id.webview);
+        webView.loadUrl("https://www.javatpoint.com/");
     }
 }
--- app/src/main/java/com/example/noteapp/persistance/NoteDatabase.java
+++ app/src/main/java/com/example/noteapp/persistance/NoteDatabase.java
@@ -2,10 +2,13 @@ package com.example.noteapp.persistance;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import com.example.noteapp.async.PopulateDbAsyncTask;
 import com.example.noteapp.model.Note;
 
 @Database(entities={Note.class},version = 1)
@@ -15,11 +18,18 @@ public abstract class NoteDatabase extends RoomDatabase {
         if(instance==null){
        instance= Room.databaseBuilder(context.getApplicationContext(),
                NoteDatabase.class,"note_database")
+               .addCallback(roomCallBack)
                .fallbackToDestructiveMigration()
                .build();
         }
         return  instance;
     }
     public  abstract NoteDao noteDao();
-
+private static RoomDatabase.Callback roomCallBack=new RoomDatabase.Callback(){
+    @Override
+    public void onCreate(@NonNull SupportSQLiteDatabase db) {
+        super.onCreate(db);
+        new PopulateDbAsyncTask(instance).execute();
+    }
+};
 }
