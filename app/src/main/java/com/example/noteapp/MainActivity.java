6148cb258c3689fa9decdee235628d5ab9c17984
dipak <sahdeepak330@gmail.com>
2020-12-14 19:04:02 +0530

    creating recyclerview

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.webkit.WebView;
 
+import com.example.noteapp.adapters.NoteAdapter;
 import com.example.noteapp.model.Note;
 
 import java.time.DayOfWeek;
@@ -15,16 +18,28 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 private NoteViewModel noteViewModel;
+private RecyclerView recyclerView;
+private NoteAdapter noteAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initRecyclerView();
         noteViewModel= ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
                 //update recyclerview
+                noteAdapter.setNotes(notes);
             }
         });
+    }
+
+    private void initRecyclerView() {
+        recyclerView=findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        noteAdapter=new NoteAdapter();
+        recyclerView.setAdapter(noteAdapter);
     }
 }
