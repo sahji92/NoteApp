b82219f920858102ef6df3e1c74806f07f5eaef1
dipak <sahdeepak330@gmail.com>
2020-12-14 16:49:15 +0530

    creating viewmodel,observe livedata on mainactivity

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 import android.webkit.WebView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.noteapp.model.Note;
 
+import java.time.DayOfWeek;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+private NoteViewModel noteViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView webView=findViewById(R.id.webview);
-        webView.loadUrl("https://www.javatpoint.com/");
+        noteViewModel= ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update recyclerview
+            }
+        });
     }
 }
