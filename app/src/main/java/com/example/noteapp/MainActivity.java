d526bb607cf96faf182fbc9f7f5a43c9305e8b79
dipak <sahdeepak330@gmail.com>
2020-12-15 13:12:17 +0530

    sending data from addNoteactivity to mainactivity via intent extras,edit menifest files

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.noteapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -7,25 +8,39 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import com.example.noteapp.adapters.NoteAdapter;
 import com.example.noteapp.model.Note;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.time.DayOfWeek;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-private NoteViewModel noteViewModel;
-private RecyclerView recyclerView;
-private NoteAdapter noteAdapter;
+    private NoteViewModel noteViewModel;
+    private RecyclerView recyclerView;
+    private NoteAdapter noteAdapter;
+    public static final int request_code = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+                startActivityForResult(intent, request_code);
+            }
+        });
         initRecyclerView();
-        noteViewModel= ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
@@ -36,10 +51,25 @@ private NoteAdapter noteAdapter;
     }
 
     private void initRecyclerView() {
-        recyclerView=findViewById(R.id.recycler_view);
+        recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
-        noteAdapter=new NoteAdapter();
+        noteAdapter = new NoteAdapter();
         recyclerView.setAdapter(noteAdapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == request_code && resultCode == RESULT_OK) {
+            String title = data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description = data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority = data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY,1);
+            Note note=new Note(title,description,priority);
+            noteViewModel.insert(note);
+            Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Note not saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
